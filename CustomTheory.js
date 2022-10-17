e47e3200a64ffca75d7f97e5028d2fc7d3f3fbf7
tredec <98469349+tredec@users.noreply.github.com>
2022-10-17 10:06:31 +0200

    Hopefully added it fr now

--- CustomTheory.js
+++ CustomTheory.js
@@ -214,10 +214,10 @@ var getPrimaryEquation = () => {
 
   result += "q_2";
 
-  result += "10^{\\lfloor log_2(n-1) \\rfloor}T_n";
+  result += "10^{\\lfloor log_2(n-1) \\rfloor}T_n\\\\\\";
 
   if (diffterm.level > 0) {
-    result += "\\\\\\\\dot{q} = ";
+    result += "\\dot{q} = ";
     if (diffterm.level === 1) result += "frac{T_n - T_{n-1}}{10}";
     if (diffterm.level === 2) result += "frac{\\max(T_n - T_{n-1})}{10}";
     if (q3term.level > 0) result += "\\min(5000, sum)^{q_3}";
